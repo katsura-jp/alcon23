e2889f984940e3910425b413169d606a2ff1f2bf
katsura_jp <nok.addayo@gmail.com>
2019-08-20 16:56:32 +0900

    add model018

---
+++ models/model018.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+try:
+    from .inceptionv4 import *
+    from .metrics import ArcMarginProduct
+except:
+    from inceptionv4 import *
+    from metrics import ArcMarginProduct
+
+
+class InceptionV4GRU2(nn.Module):
+    def __init__(self, num_classes, hidden_size=512, bidirectional=False, dropout=0.5, load_weight=None):
+        super(InceptionV4GRU2, self).__init__()
+        self.num_classes = num_classes
+        self.hidden_size = hidden_size
+        self.bidirectional = bidirectional
+
+        self.backbone = inceptionv4(num_classes=1000, pretrained='imagenet')
+        self.features = self.backbone.features
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(p=0.5)
+
+        if bidirectional:
+            self.gru1 = nn.GRU(self.backbone.last_linear.in_features, hidden_size=hidden_size//2, batch_first=True,bidirectional=True)
+            self.gru2 = nn.GRU(hidden_size, hidden_size=hidden_size//2, batch_first=True,bidirectional=True)
+        else:
+            self.gru1 = nn.GRU(self.backbone.last_linear.in_features, hidden_size=hidden_size, batch_first=True, bidirectional=False)
+            self.gru2 = nn.GRU(hidden_size, hidden_size=hidden_size, batch_first=True, bidirectional=False)
+
+        # self.fc = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU(inplace=True)
+        self.arc_margin_product = ArcMarginProduct(in_features=hidden_size, out_features=num_classes,
+                                                   s=30.0, m=0.50, easy_margin=False)
+
+    def encode(self, x):
+        x = self.features(x)
+        # x = self.relu(x)
+        x = self.avg_pool(x)
+        if self.dropout is not None:
+            x = self.dropout(x)
+        x = x.view(x.size(0), -1)
+        return x
+
+    def decode(self, x):
+        batch, seq, _ = x.size()
+        if self.bidirectional:
+            hidden = torch.zeros(2, batch, self.hidden_size//2, dtype=x.dtype, device=x.device)
+        else:
+            hidden = torch.zeros(1, batch, self.hidden_size, dtype=x.dtype, device=x.device)
+
+        x, _ = self.gru1(x, hidden)
+        x, _ = self.gru2(x, hidden)
+
+        # x = self.fc(x.contiguous().view(-1, x.size(2)))
+        x = x.contiguous().view(-1, x.size(2))
+        return x
+
+    def forward(self, x, label):
+        b, s, c, h, w = x.size()
+        x = self.encode(x.view(-1, c, h, w))
+        x = self.decode(x.view(b, s, -1))
+        x = self.arc_margin_product(x, label)
+        return x
+
+    def feature(self, x):
+        b, s, c, h, w = x.size()
+        x = self.encode(x.view(-1, c, h, w))
+        x = self.decode(x.view(b, s, -1))
+        return x
+
+
+def test():
+    import random
+    # inputs = torch.FloatTensor(6, 3, 3,200, 200)
+    inputs = torch.FloatTensor(16, 3, 3, 192 , 128)
+    label = torch.tensor([random.randint(0,47) for _ in range(16*3)])
+    model = InceptionV4GRU2(48, 512, bidirectional=True)
+    label = torch.eye(48)[label]
+    # model = model.to('cuda:0')
+    # inputs = inputs.to('cuda:0')
+    logits = model(inputs, label)
+    print(logits.size())
+    assert logits.size() == (16 * 3, 48)
+
+if __name__ == '__main__':
+    test()
