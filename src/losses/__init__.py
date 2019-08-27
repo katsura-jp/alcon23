bd5b715e751607709a1251e82a65bb15e361313d
katsura-jp <nok.addayo@gmail.com>
2019-08-27 01:53:40 +0000

    rename margin loss

--- src/losses/__init__.py
+++ src/losses/__init__.py
@@ -2,5 +2,4 @@ from .focal_loss import FocalLoss
 from .tversky_loss import TverskyLoss
 from .cross_entropy_loss_without_softmax import CrossEntropyLossWithoutSoftmax
 from .label_smooth_loss import LabelSmoothLoss
-from .margin_cross_entropy import MarginCrossEntropy
-
+from .margin_cross_entropy import MarginCrossEntropyLoss
